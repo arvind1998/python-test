8ee55b9f679f86daa91c20bda6c6f6f615c95841
Arvind Ramadurai <arvind.r@browserstack.com>
2021-09-06 20:53:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,17 +21,17 @@ desired_cap['build'] = os.environ.get('BROWSERSTACK_BUILD_NAME')
 driver = webdriver.Remote(
     command_executor='https://' + username + ':' + password + '@hub-cloud.browserstack.com/wd/hub',
     desired_capabilities=desired_cap)
-driver.get("http://localhost:3000")
-# if not "Google" in driver.title:
-#     raise Exception("Unable to load google page!")
-# elem = driver.find_element_by_name("q")
-# elem.send_keys("BrowserStack")
-# elem.submit()
-# try:
-#     WebDriverWait(driver, 5).until(EC.title_contains("BrowserStack"))
-#     # Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page starts with 'BrowserStack'
-#     driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Title matched!"}}')
-# except TimeoutException:
-#     driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Title not matched"}}')
+driver.get("https://www.google.com")
+if not "Google" in driver.title:
+    raise Exception("Unable to load google page!")
+elem = driver.find_element_by_name("q")
+elem.send_keys("BrowserStack")
+elem.submit()
+try:
+    WebDriverWait(driver, 5).until(EC.title_contains("BrowserStack"))
+    # Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page starts with 'BrowserStack'
+    driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "Title matched!"}}')
+except TimeoutException:
+    driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Title not matched"}}')
 print(driver.title)
 driver.quit() 
